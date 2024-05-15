f96e91af34c459b62a19cea4c10319c6a15d803e
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2024-05-15 12:36:58 -0300

    adicionando classe Data

---
+++ questão03/Data.java
@@ -0,0 +1,44 @@
+package questão03;
+
+public class Data {
+    int dia;
+    int mes;
+    int ano;
+
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public void displayData() {
+        String data = String.format("%d/%d/%d", dia, mes, ano);
+        System.out.println(data);
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    
+}
