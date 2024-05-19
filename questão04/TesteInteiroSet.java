54543391282ace65a4723906524f96e0fb140a68
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2024-05-19 12:00:16 -0300

    finalizando prova

--- questão04/TesteInteiroSet.java
+++ questão04/TesteInteiroSet.java
@@ -1,5 +1,7 @@
 package questão04;
 
+import java.util.Arrays;
+
 public class TesteInteiroSet {
     
     public static void main(String[] args) {
@@ -7,10 +9,16 @@ public class TesteInteiroSet {
         InteiroSet conjunto1 = new InteiroSet();
         conjunto1.insereElemento(0);
         conjunto1.insereElemento(100);
+        boolean[] uniao = conjunto1.union(conjunto1.conjunto, conjuntoVazio.conjunto);
+        boolean[] interseccao = conjunto1.interseccao(conjunto1.conjunto, conjuntoVazio.conjunto);
 
         System.out.println("Conjunto vazio: " + conjuntoVazio.toSetString());
         System.out.println("Conjunto 1: " + conjunto1.toSetString());
 
-        System.out.println("Uniao: " + conjunto1.toSetString());
+        System.out.println("Uniao: " + Arrays.toString(uniao));
+        System.out.println("Uniao: " + Arrays.toString(interseccao));
+
+        System.out.println("Conjunto 1 eh igual ao conjunto vazio? " + conjunto1.ehIgualTo(conjunto1.conjunto, conjuntoVazio.conjunto));
+        System.out.println("Conjunto 1 eh igual ao conjunto 1? " + conjunto1.ehIgualTo(conjunto1.conjunto, conjunto1.conjunto));
     }
 }
